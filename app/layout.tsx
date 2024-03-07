0441faa51cebd1376723a27cd9b194b3f5d4eabc
Philip Bendiksen <philipbendiksen@gmail.com>
2024-03-07 09:26:07 +0100

    Installed MUI and comment out theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import { LayoutProps } from "./types";
 import Header from "./components/header";
-import { ThemeProvider, createTheme } from "@mui/material";
-import { green } from "@mui/material/colors";
+import { LayoutProps } from "./types";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,25 +12,25 @@ export const metadata: Metadata = {
   description: "Dina favoritprodukter online till en bra pris...",
 };
 
-const theme = createTheme({
+/* const theme = createTheme({
   palette: { primary: { main: "green" } },
-});
+}); */
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider theme={theme}>
-          <Header />
-          <Link href="/">
-            <h1>Ice</h1>
-          </Link>
+        {/* <ThemeProvider theme={theme}> */}
+        <Header />
+        <Link href="/">
+          <h1>Ice</h1>
+        </Link>
 
-          {children}
-          <footer>
-            <p>NAPS © 2024</p>
-          </footer>
-        </ThemeProvider>
+        {children}
+        <footer>
+          <p>NAPS © 2024</p>
+        </footer>
+        {/*   </ThemeProvider> */}
       </body>
     </html>
   );
