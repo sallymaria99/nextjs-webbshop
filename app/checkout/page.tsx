0e1b06639a94872e64e163f1b6f01d858a1f90d8
Nathalie <lysefeldt@outlook.com>
2024-03-18 10:30:48 +0100

    refactory checkoutpage
    Co-authored-by: Annamedie <Annamedie@users.noreply.github.com>
    Co-authored-by: sallymaria99 <sallymaria99@users.noreply.github.com>

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,55 +1,15 @@
-"use client";
-import { Box, Container, Stack, Typography } from "@mui/material";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import DeleteButton from "../components/deleteButton";
-import HandleQuantbutton from "../components/quantbutton";
-import { useCart } from "../contexts/CartContext";
+import CartSummary from "./CartSummary";
 import Checkout from "./Checkout";
 
 function CheckoutPage() {
-	const { cart } = useCart();
-	const [totalPrice, setTotalPrice] = useState(0);
-
-	useEffect(() => {
-		const itemCount: any = cart.reduce(
-			(total, item) => total + item.quantity * item.price,
-			0
-		);
-		setTotalPrice(itemCount);
-	}, [cart]);
-	console.log(totalPrice);
 	return (
 		<main>
-			<Box component="div">
-				<h1>{totalPrice}</h1>
-			</Box>
-			{cart.length > 0 ? (
-				cart.map((cartItem) => (
-					<Container key={cartItem.id}>
-						<Stack direction={"row"}>
-							<Typography variant="h2">{cartItem.title}</Typography>{" "}
-							<Typography variant="h4">
-								{cartItem.quantity * cartItem.price}
-							</Typography>
-							<Image
-								src={cartItem.image}
-								alt={cartItem.title}
-								width={50}
-								height={50}
-							></Image>
-						</Stack>
-						<HandleQuantbutton id={cartItem.id} />
-						<DeleteButton id={cartItem.id} />
-					</Container>
-				))
-			) : (
-				<Typography variant="h1">Din varukorg är tom.</Typography>
-			)}
+			<CartSummary />
 			<span>
 				<Checkout />
 			</span>
 		</main>
 	);
 }
+
 export default CheckoutPage;
