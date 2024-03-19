348a35b600425d32f143f88260022c44430441c7
sallymaria99 <sallymaria@live.se>
2024-03-19 10:22:30 +0100

    Confirmation-Delete Dialog

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -9,6 +9,7 @@ import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { useProducts } from "../contexts/ProductContext";
 import MultiActionAreaCard from "../components/productCard";
 import AdminDelete from "../components/adminDelete";
+import { useState } from "react";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -20,6 +21,7 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function AdminPage() {
   const { products } = useProducts();
+
   return (
     <Box component="main" data-cy="product">
       <IconButton component={Link} href="/admin/overview">
@@ -47,7 +49,3 @@ export default function AdminPage() {
     </Box>
   );
 }
-
-/**Designen på denna sida är valfri men skall utgå ifrån designsystemet ni använder er av. Det skall finnas en knapp på startsidan som tar användaren till adminsidan. På adminsidan skall ni lista alla produkter samt ge användaren möjlighet att ta bort, lägga till eller ändra samtliga produkter (CRUD). Om ni väljer att ha en separat sida, modal eller accordion för ändring/tilläggning av en produkt är valfritt men flödet ska vara routat. Samtliga produkter skall vara sparade i localstorage, detta betyder att om localstorage är tomt vid inladdning av sidan behöver samtliga fördefinierade produkter sparas till localstorage. URL används för bilder så det enkelt kan sparas i localstorage.
-
-Produktlistan måste döpas till `products` i localStorage. */
