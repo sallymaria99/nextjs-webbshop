3262eef0e2892ac58c26a73916da54dcfb39fd38
Nathalie <lysefeldt@outlook.com>
2024-03-19 23:22:13 +0100

    responsiveness in page for introtext and add new picture to include

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Box, Grid, Typography } from "@mui/material";
+import Image from "next/image";
 import MultiActionAreaCard from "./components/productCard";
 import { useProducts } from "./contexts/ProductContext";
 
@@ -13,32 +14,56 @@ function Home() {
 				width: "100%",
 				maxWidth: "100vw",
 				overflowX: "hidden",
+				minHeight: "100vh",
 				backgroundImage: `url("/backgroundHome.jpg")`,
 				backgroundSize: "cover",
 				backgroundPosition: "center",
 				backgroundRepeat: "no-repeat",
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
 			}}
 		>
 			<Grid
 				container
-				spacing={1}
+				spacing={2}
 				justifyContent="center"
 				sx={{
-					backgroundColor: "rgba(255, 255, 255, 0.7)",
-					borderRadius: 4,
-					boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-					width: "400px",
-					height: "auto",
-					margin: 0,
+					marginTop: "20px",
+					marginBottom: "20px",
 				}}
 			>
-				<Typography>
-					Experience the pinnacle of luxury ice at Luxice. Crafted in
-					Jukkasjärvi, Sweden since 2002, our exclusive collection defines
-					excellence in every chill. Elevate your moments with only the best –
-					discover Luxice.
-				</Typography>
+				<Grid item xs={12} md={8}>
+					<Box
+						sx={{
+							backgroundColor: "rgba(255, 255, 255, 0.5)",
+							borderRadius: 4,
+							boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+							width: { xs: "90%", sm: "80%", md: "70%", lg: "80%", xl: "90%" },
+							maxWidth: "90%",
+							padding: "20px",
+							display: "flex",
+							flexDirection: { xs: "column", md: "row" },
+							alignItems: "center",
+						}}
+					>
+						<Image
+							src="/createice.png"
+							alt="createice"
+							width={150}
+							height={100}
+						/>
+						<Typography>
+							Experience the pinnacle of luxury ice at Luxice. Crafted in
+							Jukkasjärvi, Sweden since 2002, our exclusive collection defines
+							excellence in every chill. Elevate your moments with only the best
+							– discover Luxice.
+						</Typography>
+					</Box>
+				</Grid>
 			</Grid>
+
 			<Grid container spacing={2} justifyContent="center">
 				{products.map((product) => (
 					<Grid
