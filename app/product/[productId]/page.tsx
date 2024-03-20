8eeed969eee74dbff1dcc330d6b25b388f34698a
annaa <annaalehed@hotmail.com>
2024-03-20 14:17:11 +0100

    toast flyttad börjat cy-prod

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,32 +1,39 @@
 import AddToCartButton from "@/app/components/addToCartButton";
 import { Product, products } from "@/data";
+import { Typography } from "@mui/material";
 import Image from "next/image";
 
 type PageProps = { params: { productId: string } };
 
 function ProductPage({ params }: PageProps) {
-	const productId = params.productId;
-	const selectedProduct = products.find(
-		(product: Product) => product.id === productId
-	);
+  const productId = params.productId;
+  const selectedProduct = products.find(
+    (product: Product) => product.id === productId
+  );
 
-	if (!selectedProduct) {
-		return <h1>Product not found</h1>;
-	}
+  if (!selectedProduct) {
+    return <h1>Product not found</h1>;
+  }
 
-	return (
-		<div>
-			<h1 data-cy="product-title"> {selectedProduct.title}</h1>
-			<Image
-				src={selectedProduct.image}
-				alt={selectedProduct.title}
-				width={200}
-				height={200}
-			/>
-			<p data-cy="product-description">{selectedProduct.description}</p>
-			<h3 data-cy="product-title">{selectedProduct.price}</h3>
-			<AddToCartButton product={selectedProduct} />
-		</div>
-	);
+  return (
+    <main>
+      <Typography variant="h1" data-cy="product-title">
+        {selectedProduct.title}
+      </Typography>
+      <Image
+        src={selectedProduct.image}
+        alt={selectedProduct.title}
+        width={200}
+        height={200}
+      />
+      <Typography variant="body1" data-cy="product-description">
+        {selectedProduct.description}
+      </Typography>
+      <Typography variant="h3" data-cy="product-price">
+        {selectedProduct.price}
+      </Typography>
+      <AddToCartButton product={selectedProduct} />
+    </main>
+  );
 }
 export default ProductPage;
