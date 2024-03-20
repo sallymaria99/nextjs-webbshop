b90e371312034ec54b4ffa5e53f08e442946e615
Nathalie <lysefeldt@outlook.com>
2024-03-20 14:11:03 +0100

    adress on right place

--- app/confirmationPage/page.tsx
+++ app/confirmationPage/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Copyright } from "@mui/icons-material";
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -27,36 +26,10 @@ export default function ConfirmationPage() {
 		setTotalPrice(itemCount);
 	}, [cart]);
 
+	const addresses = ["1 MUI Drive", "Reactville", "Anytown", "99999", "USA"];
+
 	return (
 		<React.Fragment>
-			<Typography variant="h6" gutterBottom>
-				Order summary
-			</Typography>
-			<List disablePadding>
-				{cart.map((item) => (
-					<ListItem key={item.title} sx={{ py: 1, px: 0 }}>
-						<ListItemText primary={item.title} secondary={item.description} />
-						<Typography variant="body2">
-							{item.price * item.quantity}
-						</Typography>
-					</ListItem>
-				))}
-				<ListItem sx={{ py: 1, px: 0 }}>
-					<ListItemText primary="Total" />
-					<Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-						{totalPrice}
-					</Typography>
-				</ListItem>
-			</List>
-			<Grid container spacing={2}>
-				<Grid item xs={12} sm={6}>
-					<Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-						Shipping
-					</Typography>
-					<Typography gutterBottom>John Smith</Typography>
-					<Typography gutterBottom>{addresses.join(", ")}</Typography>
-				</Grid>
-			</Grid>
 			<CssBaseline />
 			<AppBar
 				position="absolute"
@@ -81,23 +54,60 @@ export default function ConfirmationPage() {
 					<Typography component="h1" variant="h4" align="center">
 						Order confirmation
 					</Typography>
-
-					<React.Fragment>
-						<Typography variant="subtitle1" gutterBottom>
-							Your satisfaction is our top priority, and we are committed to
-							delivering only the finest quality products to your doorstep. Our
-							team will be working diligently to ensure that your order will be
-							processed promptly and delivered to you on time. Thank you for
-							buying our quality ice, we look forward in serving you once again
-							in the future.
-						</Typography>
-						<Typography variant="subtitle2">
-							Your order number is #2001539. We have emailed your order
-							confirmation, and will send you an update when your order has
-							shipped. Have an splendid day.
-						</Typography>
-					</React.Fragment>
-					<Copyright />
+					<Grid container spacing={2}>
+						<Grid item xs={12} sm={6}>
+							<Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+								Shipping
+							</Typography>
+							<Typography gutterBottom>John Smith</Typography>
+							<Typography gutterBottom>{addresses.join(", ")}</Typography>
+							<Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+								Payment details
+							</Typography>
+							<List disablePadding>
+								{cart.map((item) => (
+									<ListItem key={item.title} sx={{ py: 1, px: 0 }}>
+										<ListItemText
+											primary={item.title}
+											secondary={item.description}
+										/>
+										<Typography variant="body2">
+											{item.price * item.quantity}
+										</Typography>
+									</ListItem>
+								))}
+								<ListItem sx={{ py: 1, px: 0 }}>
+									<ListItemText
+										primary={
+											<Typography
+												variant="subtitle1"
+												sx={{ fontWeight: "bold", marginRight: "0px" }}
+											>
+												Totalprice
+											</Typography>
+										}
+									/>
+									<Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+										{totalPrice}
+									</Typography>
+								</ListItem>
+							</List>
+						</Grid>
+						{/* Additional grid items for other content if needed */}
+					</Grid>
+					<Typography variant="subtitle1" gutterBottom>
+						Your satisfaction is our top priority, and we are committed to
+						delivering only the finest quality products to your doorstep. Our
+						team will be working diligently to ensure that your order will be
+						processed promptly and delivered to you on time. Thank you for
+						buying our quality ice, we look forward in serving you once again in
+						the future.
+					</Typography>
+					<Typography variant="subtitle2">
+						Your order number is #2001539. We have emailed your order
+						confirmation, and will send you an update when your order has
+						shipped. Have an splendid day.
+					</Typography>
 				</Paper>
 			</Container>
 		</React.Fragment>
@@ -105,5 +115,3 @@ export default function ConfirmationPage() {
 }
 
 //import console from "console";
-
-const addresses = ["1 MUI Drive", "Reactville", "Anytown", "99999", "USA"];
