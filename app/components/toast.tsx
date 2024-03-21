8ffdf3cac52ea9d231bec2cd465a537c00dc9294
Nathalie <lysefeldt@outlook.com>
2024-03-21 11:50:13 +0100

    fixed seconds for autoduration

--- app/components/toast.tsx
+++ app/components/toast.tsx
@@ -3,31 +3,31 @@ import Snackbar from "@mui/material/Snackbar";
 import React from "react";
 
 function Alert(props: AlertProps) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
+	return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
 interface ToastProps {
-  open: boolean;
-  message: string;
-  onClose: () => void;
+	open: boolean;
+	message: string;
+	onClose: () => void;
 }
 
 const Toast: React.FC<ToastProps> = ({ open, message, onClose }) => {
-  return (
-    <Snackbar
-      open={open}
-      autoHideDuration={6000}
-      onClose={onClose}
-      data-cy="added-to-cart-toast"
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-    >
-      <div>
-        <Alert onClose={onClose} severity="success">
-          {message}
-        </Alert>
-      </div>
-    </Snackbar>
-  );
+	return (
+		<Snackbar
+			open={open}
+			autoHideDuration={3000}
+			onClose={onClose}
+			data-cy="added-to-cart-toast"
+			anchorOrigin={{ vertical: "top", horizontal: "right" }}
+		>
+			<div>
+				<Alert onClose={onClose} severity="success">
+					{message}
+				</Alert>
+			</div>
+		</Snackbar>
+	);
 };
 
 export default Toast;
